80c681a852425f4f09fb83d03d417894d4ece08f
chiman <1164617068@qq.com>
2024-08-05 19:07:09 +0800

    finished part2

--- src/section/SecondPart/index.tsx
+++ src/section/SecondPart/index.tsx
@@ -1,9 +1,38 @@
 import { FC } from "react";
+import Cartoon from "../../assets/cartoon.jpg"
+import Food from "../../assets/food.jpg"
+import Movie from "../../assets/movie.png"
+import Life from "../../assets/life.jpg"
+import styles from "./styles.module.scss"
 
 const SecondPart: FC = () => {
     return (
-        <div>
-            首图
+        <div className={styles.secondPart}>
+            <ul>
+                <li>动画</li>
+                <li>美食</li>
+                <li>电影</li>
+                <li>生活</li>
+            </ul>
+
+            <div>
+                <section>
+                    <h2>动画</h2>
+                    <img src={Cartoon} />
+                </section>
+                <section>
+                    <h2>美食</h2>
+                    <img src={Food} />
+                </section>
+                <section>
+                    <h2>电影</h2>
+                    <img src={Movie} />
+                </section>
+                <section>
+                    <h2>生活</h2>
+                    <img src={Life} />
+                </section>
+            </div>
         </div>
     )
 }
