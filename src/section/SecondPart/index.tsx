b80e1e408958fa985f74840bb10baf63f6ac166e
chiman <1164617068@qq.com>
2024-08-07 17:18:42 +0800

    Version1 finished.

--- src/section/SecondPart/index.tsx
+++ src/section/SecondPart/index.tsx
@@ -1,61 +1,94 @@
-import { FC, useState } from "react";
+import { FC, useState, useRef, useEffect } from "react";
 import Cartoon from "../../assets/cartoon.jpg"
 import Food from "../../assets/food.jpg"
 import Movie from "../../assets/movie.png"
 import Life from "../../assets/life.jpg"
+import Logo from "../../assets/logo.png"
 import styles from "./styles.module.scss"
 
 const SecondPart: FC = () => {
     const [activeTab, setActiveTab] = useState<string>("cartoon")
+    const [isFixed, setIsFixed] = useState<boolean>(false)
+    const secondPartRef = useRef<HTMLDivElement>(null)
+    const TAB_HEIGHT = 56
+    const activate = (key: string) => {
+        setActiveTab(key)
+        const el = document.getElementById(key)
+        if(el){
+            el.scrollIntoView({behavior: "smooth"})
+        }
+    }
+
+    const onScroll = () => {
+        if(secondPartRef.current){
+            let { top } = secondPartRef.current.getBoundingClientRect()
+            setIsFixed( top <= 0 )
+
+            const sectionsList = secondPartRef.current.querySelectorAll("section")
+            Array.from(sectionsList).forEach(node => {
+                const { top } = node.getBoundingClientRect()
+                const key = node.id
+                if(top <= TAB_HEIGHT ){
+                    setActiveTab(key)
+                }
+            });
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll)
+        return () => {
+            window.removeEventListener("scroll", onScroll)
+        }
+    }, [])
 
     const tabs = [
         {
             key: "cartoon",
-            title: "卡通"
+            title: "卡通",
+            imgSrc: Cartoon
         },
         {
             key: "food",
-            title: "美食"
+            title: "美食",
+            imgSrc: Food
         },
         {
             key: "movie",
-            title: "电影"
+            title: "电影",
+            imgSrc: Movie
         },
         {
             key: "life",
-            title: "生活"
+            title: "生活",
+            imgSrc: Life
         }
 
     ]
 
     return (
-        <div className={styles.secondPart}>
-            <ul>
+        <div className={styles.secondPart} ref={secondPartRef}>
+            <ul className={`${isFixed && styles.isFixed}`}>
                 {tabs.map( tab => (
-                    <li key={tab.key } onClick={()=> setActiveTab(tab.key)} >
+                    <li key={tab.key } onClick={()=> activate(tab.key)} >
                         <span>{tab.title}</span>
-                        {activeTab === tab.key && <span className={styles.line}></span>}
+                        <span className={`${styles.line} ${activeTab === tab.key && styles.visible}`}></span>
                     </li>
                 ))}
             </ul>
-
             <div>
-                <section>
-                    <h2>动画</h2>
-                    <img src={Cartoon} />
-                </section>
-                <section>
-                    <h2>美食</h2>
-                    <img src={Food} />
-                </section>
-                <section>
-                    <h2>电影</h2>
-                    <img src={Movie} />
-                </section>
-                <section>
-                    <h2>生活</h2>
-                    <img src={Life} />
-                </section>
+                {tabs.map( tab => (
+                     <section id={tab.key} key={tab.key}>
+                        <h2>{tab.title}</h2>
+                        <img src={tab.imgSrc} />
+                    </section>
+                ))}
+            </div>
+            <div className={`${styles.btnWrapper} ${isFixed && styles.visible}`}>
+                <img src={Logo} />
+                <a href="https://www.bilibili.com/" target="_blank" >
+                    <button>App 内打开</button>     
+                </a>
             </div>
         </div>
     )
